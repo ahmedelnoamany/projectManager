f2ce0eeb6b563ea1bfa0e25bce959ca1cabc2d24
ahmedelnoamany <ahmedgouda9484@gmail.com>
2018-09-19 10:04:10 -0500

    Set up apollo client. Set up react-router.

--- client/index.js
+++ client/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
 import App from './src/containers/app/App';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache,
+  dataIdFromObject: o => o.id
+});
+
 const Root = () => {
   return (
-    <App />
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('.container'));
+ReactDOM.render(<Root />, document.querySelector('#root'));
